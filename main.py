9961551ea4c12e9ad62809ea0758b6dfeb4302e3
Robert <mynamedark713@gmail.com>
2025-03-26 14:34:06 +0200

    fig bug with unknown sender, and and max values for summary length and max messages limit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import asyncio
+import traceback
 from auto_summary_collector import AutoSummaryCollector
 from telethon import TelegramClient, events
 
 from config import summary_generator_config as config
 from telethon_user_api_tools import generate_summary_by_messages, client
 
-
-bot_session_name = f"{config.sessions_path}/{config.bot_session_name}"
-bot = TelegramClient(bot_session_name, config.telegram_api_id, config.telegram_api_hash)
 
 summary_collector = AutoSummaryCollector()
 
@@ -16,18 +14,37 @@ summary_collector = AutoSummaryCollector()
 async def summary_handler(event):
     limit_str = event.pattern_match.group(1)
     summary_length_str = event.pattern_match.group(2)
+    chat_id = event.chat_id
     # Check if length of summary passed correct
     try:
         summary_length = int(summary_length_str) if summary_length_str else config.default_summary_length
     except ValueError:
         summary_length = config.default_summary_length
+
+    # Check if summary length too long
+    if summary_length > config.max_summary_length:
+        summary_length = config.max_summary_length
+        await client.send_message(
+            chat_id,
+            message="The summary length exceeds the maximum allowed "
+                     f"limit of {config.max_summary_length} characters. "
+                     f"It has been adjusted to {config.max_summary_length}.")
     # Check if number of messages passed correct
     try:
         limit = int(limit_str)
     except ValueError:
         limit = 50
+    # Check if messages limit too long
+    # Check if messages limit too long
+    if limit > config.max_messages_to_gather:
+        limit = config.max_messages_to_gather
+        await client.send_message(
+            chat_id,
+            message="The number of messages exceeds the maximum allowed "
+                     f"limit of {config.max_messages_to_gather}. "
+                     f"It has been adjusted to {config.max_messages_to_gather}."
+        )
 
-    chat_id = event.chat_id
     summary = await generate_summary_by_messages(chat_id, limit, summary_length)
     # Reply to the user message
     await event.reply(message=summary)
@@ -47,6 +64,16 @@ async def set_auto_summary_handler(event):
         number_of_messages = int(number_of_messages_str)
     except ValueError:
         number_of_messages = 100
+    # Check if messages limit too long
+
+    if number_of_messages > config.max_messages_to_gather:
+        number_of_messages = config.max_messages_to_gather
+        await client.send_message(
+            chat_id,
+            message=f"The number of messages exceeds the maximum allowed "
+                     f"limit of {config.max_messages_to_gather}. "
+                     f"It has been adjusted to {config.max_messages_to_gather}."
+        )
     chat_id = event.chat_id
     summary_collector.start_collect_messages(chat_id=chat_id, messages_per_collect=number_of_messages)
     await event.reply(f"Auto-summarization enabled with a threshold of {number_of_messages} messages.")
@@ -76,13 +103,18 @@ async def auto_summary_collector(event):
     if summary_collector.is_full(chat_id):
         summary = summary_collector.generate_summary(chat_id)
         message = f"Summarization of last {chat.messages_per_collect} messages:\n\n{summary}"
-        await bot.send_message(chat_id, message=message)
+        await client.send_message(chat_id, message=message)
 
 
 async def main():
     await client.start()
     print("Bot is working")
-    await client.run_until_disconnected()
+    while True:
+        try:
+            await client.run_until_disconnected()
+        except:
+            traceback.print_exc()
+            await asyncio.sleep(10)
 
 
 if __name__ == "__main__":
